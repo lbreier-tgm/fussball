b0fb420f3473ef97cbb3de51d1395bc256f74932
Lorenz Breier <lorenz@Lorenzs-MacBook-Pro.local>
2016-04-01 08:56:39 +0200

    Protokollbearbeitungen - CREATE Skript vollständig (Überprüfung auf Richtigkeit noch notwendig)

--- sql/create.sql
+++ sql/create.sql
@@ -14,19 +14,19 @@ CREATE TABLE person (
 );
 
 CREATE TABLE angestellter(
-  persnr INTEGER PRIMARY KEY REFERENCES person,
+  persnr SERIAL PRIMARY KEY REFERENCES person,
   ueberstunden NUMERIC(3,0) NOT NULL,
   gehalt NUMERIC(8,2) NOT NULL,
   e_mail VARCHAR(50) NOT NULL,
 );
 
 CREATE TABLE mitglied (
-  persnr INTEGER PRIMARY KEY REFERENCES person,
+  persnr SERIAL PRIMARY KEY REFERENCES person,
   beitrag NUMERIC(8,2) NOT NULL
 );
 
 CREATE TABLE spieler (
-  persnr INTEGER PRIMARY KEY REFERENCES person,
+  persnr SERIAL PRIMARY KEY REFERENCES person,
   position VARCHAR(20) NOT NULL,
   gehalt NUMERIC(8,2) NOT NULL,
   von DATE NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE spieler (
 );
 
 CREATE TABLE trainer (
-  persnr INTEGER PRIMARY KEY REFERENCES person,
+  persnr SERIAL PRIMARY KEY REFERENCES person,
   gehalt NUMERIC(8,2) NOT NULL,
   von DATE NOT NULL,
   bis DATE NOT NULL
@@ -48,9 +48,9 @@ CREATE TABLE mannschaft (
   FOREIGN KEY(persnr) REFERENCES spieler
 );
 
-# WIE GEHT UNIQUE
+
 CREATE TABLE ist_trainer_von (
-  trainernr INTEGER FOREIGN KEY REFERENCES person.persnr UNIQUE,
+  trainernr SERIAL FOREIGN KEY REFERENCES person.persnr UNIQUE,
   manbez VARCHAR(40) FOREIGN KEY REFERENCES mannschaft.bezeichnung UNIQUE,
   trainertyp BOOLEAN UNIQUE,
   PRIMARY KEY(trainernr, manbez, trainertyp)
@@ -59,9 +59,36 @@ CREATE TABLE ist_trainer_von (
 
 CREATE TABLE mannschaftspieler (
   bezeichnung VARCHAR(40),
-  persnr INTEGER PRIMARY KEY REFERENCES spieler,
-  nummer NUMERIC(2,0)
+  persnr SERIAL PRIMARY KEY REFERENCES spieler,
+  nummer NUMERIC(2,0),
   PRIMARY KEY(persnr),
   FOREIGN KEY(persnr) REFERENCES spieler
 );
 
+CREATE TABLE standort (
+  sid INTEGER PRIMARY KEY,
+  land VARCHAR(30) NOT NULL,
+  plz VARCHAR(10) NOT NULL,
+  ort VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE fanclub (
+  name VARCHAR(30) PRIMARY KEY,
+  sid INTEGER FOREIGN KEY REFERENCES standort.sid,
+  gegruendet DATE NOT NULL
+);
+
+CREATE TABLE ist_obmann (
+  persnr SERIAL FOREIGN KEY REFERENCES mitglied.persnr UNIQUE,
+  name VARCHAR(30) FOREIGN KEY REFERENCES fanclub.name UNIQUE,
+  sid INTEGER FOREIGN KEY REFERENCES fanclub.sid UNIQUE
+  PRIMARY KEY(persnr, name, sid)
+);
+
+CREATE TABLE betreuung (
+  name VARCHAR(30) FOREIGN KEY REFERENCES fanclub.name,
+  sid INTEGER FOREIGN KEY REFERENCES fanclub.name,
+  persnr SERIAL FOREIGN KEY REFERENCES angestellter.persnr,
+  anfang DATE NOT NULL,
+  ende DATE NOT NULL
+)
