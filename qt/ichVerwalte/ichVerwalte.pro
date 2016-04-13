5089f1ef40d7b2dcbf9989734822747658821d3d
Lorenz Breier <lorenz@Lorenzs-MacBook-Pro.local>
2016-04-13 12:10:51 +0200

    Database Connection endlich funktionierend (Library-Installation hat funktioniert)

--- qt/ichVerwalte/ichVerwalte.pro
+++ qt/ichVerwalte/ichVerwalte.pro
@@ -19,4 +19,12 @@ HEADERS  += mainwindow.h
 
 FORMS    += mainwindow.ui
 
-INCLUDEPATH = /home/Downloads/libpqxx-4.0/
+
+unix|win32: LIBS += -lpqxx-4.0
+
+win32:CONFIG(release, debug|release): LIBS += -L$$PWD/../../../../../usr/local/Cellar/libpqxx/4.0.1/lib/release/ -lpqxx-4.0
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$PWD/../../../../../usr/local/Cellar/libpqxx/4.0.1/lib/debug/ -lpqxx-4.0
+else:unix: LIBS += -L$$PWD/../../../../../usr/local/Cellar/libpqxx/4.0.1/lib/ -lpqxx-4.0
+
+INCLUDEPATH += $$PWD/../../../../../usr/local/Cellar/libpqxx/4.0.1/include
+DEPENDPATH += $$PWD/../../../../../usr/local/Cellar/libpqxx/4.0.1/include
--- qt/ichVerwalte/mainwindow.cpp
+++ qt/ichVerwalte/mainwindow.cpp
@@ -27,25 +27,66 @@ MainWindow::~MainWindow()
     bool ok = db.open();
 }*/
 
-void connectToDatabase(){
-    int main(int argc, char* argv[])
-    {
-       try{
-          connection C("dbname=verein user=manager password=iderfrein \
-          hostaddr=127.0.0.1 port=5432");
-          if (C.is_open()) {
-             cout << "Verbunden mit der Dantenbank : " << C.dbname() << endl;
-          } else {
-             cout << "Verbindung war nicht möglich." << endl;
-             return 1;
-          }
-          C.disconnect ();
-       }catch (const std::exception &e){
-          cerr << e.what() << std::endl;
+int connectToDatabase(){
+    try{
+       //Verbindung mit Verein herstellen - Testfunktion zum Verbindung testen.
+       connection C("dbname=verein user=manager password=iderfrein\
+       hostaddr=127.0.0.1 port=5432");
+       if (C.is_open()) {
+          cout << "Opened database successfully: " << C.dbname() << endl;
+       } else {
+          cout << "Can't open database" << endl;
           return 1;
        }
+       C.disconnect ();
+    }catch (const std::exception &e){
+       cerr << e.what() << std::endl;
+       return 1;
+    }
+    return 0;
+}
+
+int updateDatabase(){
+    string sql;
+    try{
+       connection C("dbname=verein user=manager password=iderfrein\
+       hostaddr=127.0.0.1 port=5432");
+       if (C.is_open()) {
+          cout << "Opened database successfully: " << C.dbname() << endl;
+       } else {
+          cout << "Can't open database" << endl;
+          return 1;
+       }
+       C.disconnect ();
+    }catch (const std::exception &e){
+       cerr << e.what() << std::endl;
+       return 1;
+    }
+    /* Create SQL statement */
+    sql = "SELECT * FROM spieler";
+
+    /* Create a non-transactional object. */
+    nontransaction N(C);
+
+    /* Execute SQL query */
+    result R( N.exec( sql ));
+
+    /* List down all the records */
+    for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
+       cout << "ID = " << c[0].as<int>() << endl;
+       cout << "Name = " << c[1].as<string>() << endl;
+       cout << "Age = " << c[2].as<int>() << endl;
+       cout << "Address = " << c[3].as<string>() << endl;
+       cout << "Salary = " << c[4].as<float>() << endl;
+    }
+    cout << "Operation done successfully" << endl;
+    C.disconnect ();
+    } catch (const std::exception &e){
+        cerr << e.what() << std::endl;
+    return 1;
     }
 }
+
 
 void MainWindow::on_pushButton_clicked()
 {
